d68ac6b18c182d0755be585f8b38fa776b65d645
DarrelRamasray <ddramasr@syr.edu>
2026-09-01 11:09:32 -0400

    NavPage, Lab1,Lab2

--- Lab2.py
+++ Lab2.py
@@ -6,9 +6,7 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-##****
 st.title("Lab 2 - MY Document question answering")  #Updated title so I know this is the second page
-##****
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,12 @@
 #DARREL RAMASRAY
 #IST 688 - Building HC-AI Apps
-#Lab01
+#Labs
 
-##****
 import streamlit as st
 
-lab1_page = st.Page("Lab1.py", title="Lab 1", icon=":material/description:")
-lab2_page = st.Page("Lab2.py", title="Lab 2", icon=":material/description:", default=True)  #Default page
+lab1_page = st.Page("Lab1.py", title="Lab 1", icon=":material/description:", default=True)  #Default page
+lab2_page = st.Page("Lab2.py", title="Lab 2", icon=":material/description:")  #Default page
 
-pg = st.navigation([lab2_page, lab1_page])  #Lab2 listed first so it opens by default
+pg = st.navigation([lab1_page, lab2_page])  #Lab2 listed first so it opens by default
 st.set_page_config(page_title="Lab Application", page_icon=":material/edit:")
 pg.run()
-##****
