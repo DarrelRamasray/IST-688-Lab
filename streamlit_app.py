f4a9bb7fc471739455609854a225e1b49e0c9d30
DarrelRamasray <ddramasr@syr.edu>
2026-09-01 18:35:17 -0400

    Ordering Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,6 @@ import streamlit as st
 lab1_page = st.Page("Lab1.py", title="Lab 1", icon=":material/description:")
 lab2_page = st.Page("Lab2.py", title="Lab 2", icon=":material/description:", default=True)  #Default page
 
-pg = st.navigation([lab1_page, lab2_page])  #Lab2 listed first so it opens by default
+pg = st.navigation([lab2_page, lab1_page])  #Lab2 listed first so it opens by default
 st.set_page_config(page_title="Lab Application", page_icon=":material/edit:")
 pg.run()
